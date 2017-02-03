17e39f429fb2024b72464c245110a5ff42fe0ad7
blackcoat77 <kimanovic@gmail.com>
2017-02-03 14:05:44 -0500

    Move dashboard controller to users folder, Edit routes and dashboard index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    dashboard_path
+    users_dashboard_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 
-  get 'dashboard' => 'dashboard#index', as: :dashboard
+  # get 'dashboard' => 'dashboard#index', as: :dashboard
+  namespace :users do
+      get 'dashboard' => 'dashboard#index', as: :dashboard
+  end
 
   devise_for :users, controllers: {
       sessions: 'users/sessions',
